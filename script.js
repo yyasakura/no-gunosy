84e02f92ae13c1d0296a4675422bb20e5d172219
yyasakura <yuasakura@digi-popeye.jp>
2015-06-09 14:21:29 +0900

    リンク箇所変更に伴う修正
    Gunosy側のリンク箇所変更に合わせて修正。

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 /**
  * Created by sophide on 2015/03/01.
+ * Last update 2015/06/09.
  */
 (function(){
-	location.href = document.querySelector('.articles-show-click').href;
+	//location.href = document.querySelector('.articles-show-click').href;
+	location.href = document.querySelector('.app-share-link>a').href;
 })();
